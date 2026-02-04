77944b9343bc140831e32ea378d5d1f8d10765c3
Clover <mfundaemmanuel@gmail.com>
2026-02-04 16:56:42 +0200

    AC

---
+++ AC.py
@@ -0,0 +1,9 @@
+# Input
+# Taking temperature input from the user
+temperature = float(input("Enter the current temperature in °C: "))
+
+# Decision logic
+if temperature >= 20:
+    print("The temperature is suitable for wearing light clothes.")
+else:
+    print("It's better to wear warm clothes like a jacket or pullover.")
